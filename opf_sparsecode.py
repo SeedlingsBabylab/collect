bc84399db1c9a38078097527a6f9681689184c4f
Gladys Baudet <gladys.baudet@gmail.com>
2019-03-25 13:54:28 -0400

    month as argument

--- opf_sparsecode.py
+++ opf_sparsecode.py
@@ -3,17 +3,18 @@ import os
 import shutil
 
 ignore = ["old_files", "Old Files", "Old files", "Old_Files", "old_opfs"]
+#
+# months = []
 
-months = []
 
-
-def correct_month(root):
+def correct_month(root, month):
     split_root_begin = root.split("Home_Visit")[0][:-1]
     root_base = os.path.basename(split_root_begin)
 
-    if months and root_base[3:5] in months:
+    # if months and root_base[3:5] in months:
+    if month and root_base[3:5]==month:
         return True
-    elif not months:
+    elif not month:
         return True
     return False
 
@@ -38,13 +39,17 @@ if __name__ == "__main__":
 
     opf_paths = sys.argv[1]
     out = sys.argv[2]
+    month = False
+    if len(sys.argv)>3:
+        month = sys.argv[3]
 
     with open(opf_paths, 'r') as f:
         lines = f.readlines()
 
     for line in lines:
+        print(line)
         line = line.strip()
-        if correct_month(line):
+        if correct_month(line, month):
             for opf in os.listdir(line):
                 if opf.endswith(".opf"):
                     shutil.copy(os.path.join(line, opf), os.path.join(out, opf))
