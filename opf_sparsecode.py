37100e7c97e64ea68672aaf4399d0e71d89f5062
itang1 <irenetang14@gmail.com>
2019-08-13 12:41:21 -0400

    standardized usage, added annotations
    Added check for correct command-line usage.
    Added comments.
    ALWAYS takes month as an argument

--- opf_sparsecode.py
+++ opf_sparsecode.py
@@ -1,13 +1,15 @@
+# usage: python3 opf_sparsecode.py [path/to/opf_directories.txt] [path/to/out/folder/] [month]
+
 import sys
 import os
 import shutil
 
-ignore = ["old_files", "Old Files", "Old files", "Old_Files", "old_opfs"]
-#
-# months = []
-
-
 def correct_month(root, month):
+    """
+    root should be a filepath.
+    month should be a number (from argv[3])
+    Returns True if root pertains to month, False otherwise
+    """
     split_root_begin = root.split("Home_Visit")[0][:-1]
     root_base = os.path.basename(split_root_begin)
 
@@ -19,37 +21,42 @@ def correct_month(root, month):
     return False
 
 
-def crawl(start, out):
-    for root, dirs, files in os.walk(start):
-        if correct_month(root):
-            if "Coding" in root and "Video_Annotation" in root and not any(x in root for x in ignore):
-                for file in files:
-                    if file.endswith("sparse_code.opf"):
-                        print file
-                        shutil.copy(os.path.join(root, file),
-                                    os.path.join(out, file))
-
-
 if __name__ == "__main__":
+    """
+    For each of the 44 participants, a copy of their .opf video annotation
+    file in Subject Files is gatherd into another working folder in Working Files
+    """
+    
+    # checks correct command line usage
+    if len(sys.argv) != 4:
+        print('usage: python3 opf_sparsecode.py [path/to/opf_directories.txt] [path/to/out/folder/] [month]')
+        exit(-1)
 
-    # start = sys.argv[1]
-    # out = sys.argv[2]
-    #
-    # crawl(start, out)
+    # a .txt containing a list of paths to directories in which .opfs are stored
+    # i.e. opf_directories.txt
+    opf_directories = sys.argv[1]
 
-    opf_paths = sys.argv[1]
-    out = sys.argv[2]
-    month = False
-    if len(sys.argv)>3:
-        month = sys.argv[3]
+    # path to the folder of where you want to copy the .opfs to
+    # the folder needs to be created beforehand
+    out_folder = sys.argv[2]
 
-    with open(opf_paths, 'r') as f:
+    # month that you would like to copy for
+    # e.g. 13 if you are trying to do reliability checks for month 13
+    month = sys.argv[3]
+
+    # reads the .txt of paths into a list
+    with open(opf_directories, 'r') as f:
         lines = f.readlines()
 
+    # copying occurs here
     for line in lines:
-        print(line)
         line = line.strip()
+        # only copy for the indicated month
+        # (e.g. if you are trying to do reliability checks for month 13,
+        # then do not copy .opfs from other months)
         if correct_month(line, month):
             for opf in os.listdir(line):
+                # only copy the .opfs
                 if opf.endswith(".opf"):
-                    shutil.copy(os.path.join(line, opf), os.path.join(out, opf))
+                    shutil.copy(os.path.join(line, opf), os.path.join(out_folder, opf))
+                    print("copied " + line)
