e6983cddaa80d5561a57d20d38c531c4cf1786ae
Sarp Uner <sarpim@gmail.com>
2020-03-23 14:49:57 -0400

    fixes #1

--- gbl_paths.py
+++ gbl_paths.py
@@ -1,21 +1,29 @@
 import os, sys
 from shutil import copy
+import argparse
 
-def copy_files(media_list, output_directory):
-    for dir in media_list:
-        dir = dir.strip()
-        for file in os.listdir(dir):
-            if file.endswith("sparse_code.csv"):
-                copy(os.path.join(dir, file), output_dir)
+def get_args():
+    parser = argparse.ArgumentParser(description='Collect basic level files from list of input directories (files containing input directories: a path file) to output directory. You may supply as many input files containing basic level directories as you wish.')
+    parser.add_argument('dirfile', help='A file (or files) that contains basic level directories, one per line', nargs='+')
+    parser.add_argument('outdir', help='The output directory to copy all the basic level files to.')
+    return parser.parse_args()
+
+
+def copy_files(media_list, output_dir):
+    for d in media_list:
+        d = d.strip()
+        for f in os.listdir(d):
+            if f.endswith("sparse_code.csv"):
+                copy(os.path.join(d, f), output_dir)
+                break
+        else:
+            sys.stderr.write('No file copied from dir {}\n'.format(d))
+
 
 if __name__ == "__main__":
-    audio_list_file = sys.argv[1]
-    video_list_file = sys.argv[2]
-    output_dir = sys.argv[3]
+    args = get_args()
 
-    with open(audio_list_file, 'r') as f:
-        audio_list = f.readlines()
-        copy_files(audio_list, output_dir)
-    with open(video_list_file, 'r') as g:
-        video_list = g.readlines()
-        copy_files(video_list, output_dir)
+    for inf in args.dirfile:
+        with open(inf, 'r') as f:
+            bl_list = f.readlines()
+            copy_files(bl_list, args.outdir)
